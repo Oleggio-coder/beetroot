0acc9848259ea260a8b2dbe3b8b76d15fcca3318
Oleh Seredenko <olezhkaseredenko.190693@gmail.com>
2022-12-21 03:33:16 +0200

    19 December 2022 homework commit

--- 26lesson/homework/main.js
+++ 26lesson/homework/main.js
@@ -144,7 +144,7 @@ console.log('\n\nЗавдання №4\n\nДля чисел ' + c + ' та ' + d
 
 
 //5th task
-let myNumber = prompt('Введіть ціле число для знахождення усіх його дільників:', 738);
+let myNumber = prompt('Введіть ціле число для знахождення усіх його дільників:', 328);
 myNumber = Number(myNumber);
 
 console.log('\n\nЗавдання №5\n\nУсі дільники числа ' + myNumber + ' такі: \n' + Number('1') + '\n');
@@ -155,3 +155,93 @@ for (let i = 2; i * 2 <= myNumber; i++) {
   }
 }
 console.log(myNumber + '\n');
+
+
+
+//6th task
+let e = prompt('Введіть будь-яке п\'ятирозрядне число:', 12345);
+e = Number(e);
+let f = e;
+
+let fifth = e % 10; // 5
+let fourth = ((e - fifth) % 100) / 10; // 4
+let third = (((e - (fourth * 10)) - fifth) / 100) % 10; // 3
+let second = (((((e - (fourth * 10)) - fifth) / 100) - third) / 10) % 10; // 2
+let first = (e - ((second * 1000) + (third * 100) + (fourth * 10) + fifth)) / 10000; // 1
+
+let eReverse = Number(String(fifth) + String(fourth) + String(third) + String(second) + String(first));
+
+if (eReverse / e === 1) {
+  console.log('\n\nЗавдання №6\n\nЧисло ' + f + ' ПАЛІНДРОМ! (Бо обернене число ' + eReverse + ' таке ж саме).');
+} else {
+  console.log('\n\nЗавдання №6\n\nЧисло ' + f + ' НЕ ПАЛІНДРОМ! (Бо обернене число ' + eReverse + ' не є таким же самим).');
+}
+
+
+
+
+//7th task
+
+/*
+
+???
+
+*/
+
+
+
+
+//8th task
+let arr = prompt("Введіть будь ласка 10 різних цілих чисел:", [(-37),(-14),(-6),(0),(0),(1),(13),(29),(44),(60)]);
+
+let arrNormal = [ (Number(String(arr[0]) + String(arr[1]) + String(arr[2]))), 
+              (Number(String(arr[4]) + String(arr[5]) + String(arr[6]))), 
+              (Number(String(arr[8]) + String(arr[9]))), 
+              (Number(String(arr[11]))), 
+              (Number(String(arr[13]))), 
+              (Number(String(arr[15]))), 
+              (Number(String(arr[17]) + String(arr[18]))), 
+              (Number(String(arr[20]) + String(arr[21]))), 
+              (Number(String(arr[23]) + String(arr[24]))), 
+              (Number(String(arr[26]) + String(arr[27]))) 
+            ];
+
+console.log('\n\nЗавдання №8\n\nМаємо наступний масив чисел:');
+
+console.log(arrNormal);
+
+let negNumbers = 0;
+let posNumbers = 0;
+let oddNumbers = 0;
+let eveNumbers = 0;
+let zeroes = 0;
+
+for (let i = 0; i <= 9; i++) {
+  if (arrNormal[i] < 0) {
+    negNumbers = negNumbers + 1;
+    if (arrNormal[i] % 2 === 0) {
+      eveNumbers = eveNumbers + 1;
+    } else if (arrNormal[i] % 2 != 0) {
+      oddNumbers = oddNumbers + 1;
+    }
+  } else if (arrNormal[i] >= 0) {
+    if (arrNormal[i] === 0) {
+      zeroes = zeroes + 1;
+    }
+    posNumbers = posNumbers + 1;
+    if (arrNormal[i] % 2 === 0) {
+      eveNumbers = eveNumbers + 1;
+    } else if (arrNormal[i] % 2 != 0) {
+      oddNumbers = oddNumbers + 1;
+    }
+  }
+}
+
+console.log('\nУ цьому масиві:\n\nВід\'ємних чисел: ' + negNumbers + '\nДодатних чисел: ' + posNumbers + 
+            '\nНепарних чисел: ' + oddNumbers + '\nПарних чисел: ' + eveNumbers + '\nНулів: ' + zeroes);
+
+//
+
+/*
+
+*/
